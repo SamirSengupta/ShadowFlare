a6ff48c1b67e9ef805d7cdff2431609102a92df9
Samir Sengupta <Samir843301003@gmail.com>
2023-11-16 17:31:00 +0530

    1st

--- shadowflare.py
+++ shadowflare.py
@@ -2,15 +2,25 @@ import streamlit as st
 import requests
 from urllib.parse import quote_plus
 
-def get_magnet_link(search_term):
+def get_magnet_links(search_term):
     url = f'https://apibay.org/q.php?q={quote_plus(search_term)}'
     response = requests.get(url)
+    
     if response.status_code == 200:
         data = response.json()
+        
         if data:
-            magnet_link = f"magnet:?xt=urn:btih:{data[0]['info_hash']}"
-            return magnet_link
-    return "Magnet link not found"
+            # Filter out results without the necessary fields
+            valid_results = [result for result in data if all(key in result for key in ['info_hash', 'seeders', 'leechers'])]
+            
+            # Sort the valid results based on the number of seeds and peers in descending order
+            sorted_data = sorted(valid_results, key=lambda x: (int(x['seeders']), int(x['leechers'])), reverse=True)
+            
+            # Get the top 10 magnet links
+            top_10_links = [f"magnet:?xt=urn:btih:{result['info_hash']}" for result in sorted_data[:10]]
+            
+            return top_10_links
+    return ["Magnet links not found"]
 
 # Set page title and configure layout
 st.set_page_config(page_title='ShadowFlare.io', layout='wide')
@@ -23,13 +33,14 @@ st.write("Discover and retrieve magnet links for your files.")
 search_term = st.text_input('Enter the name of the file you want:')
 
 # Button to trigger the magnet link retrieval
-if st.button('Get Magnet Link'):
-    # Display a loading message while retrieving the magnet link
-    with st.spinner('Fetching Magnet Link...'):
-        magnet_url = get_magnet_link(search_term)
+if st.button('Get Top 10 Magnet Links'):
+    # Display a loading message while retrieving the magnet links
+    with st.spinner('Fetching Top 10 Magnet Links...'):
+        magnet_urls = get_magnet_links(search_term)
     
-    # Display the retrieved magnet link
-    st.write('Magnet URL:', magnet_url)
+    # Display the retrieved top 10 magnet links
+    for index, magnet_url in enumerate(magnet_urls, start=1):
+        st.write(f'Magnet URL {index}: {magnet_url}')
 
 # Footer with concise information, links, and torrenting disclaimer
 st.sidebar.write("""
